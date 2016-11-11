214113cbaba32a3d5fd11e6af7ea0c2e12763bd4
Jaz Singh <jsingh@practichem.com>
2016-11-11 17:24:12 -0600

    Add solution for 03 cat annotation

---
+++ index.js
@@ -0,0 +1,5 @@
+var picture = require("cat-picture");
+var src = picture.src;
+picture.remove();
+var image = require('lightning-image-poly');
+var viz = new image('#visualization', null, [src], {hullAlgorithm: 'convex'});
